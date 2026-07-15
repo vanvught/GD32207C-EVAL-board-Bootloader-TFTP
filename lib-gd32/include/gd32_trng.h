eb258fe2f6a9b9812c74b0e29cb80cc19bd4f609
Arjan <Arjan.van.Vught@gmail.com>
2026-07-15 15:07:06 +0200

    Add GD32 identifier support and TRNG module
    Refactors GD32 flashing/device detection to use bootloader command 0x06 identifiers, adds `--get-identifier`, improves sync/command diagnostics, and updates `gd32.json` to map chip IDs through identifier-specific series/part numbers. CI workflow is modernized (checkout/setup-python updates, all branches). Also adds a new GD32 TRNG interface and implementation for supported F20X/F4XX targets.

---
+++ lib-gd32/include/gd32_trng.h
@@ -0,0 +1,40 @@
+/**
+ * @file gd32_trng.h
+ *
+ */
+/* Copyright (C) 2026 by Arjan van Vught mailto:info@gd32-dmx.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef GD32_TRNG_H_
+#define GD32_TRNG_H_
+
+#if defined(GD32F10X) || defined(GD32F30X)
+#errror MCU is not supported
+#endif
+
+#include <cstdint>
+
+namespace gd32::trng {
+bool Init();
+bool Get(uint32_t& out);
+} // namespace gd32::trng
+
+#endif // GD32_TRNG_H_
